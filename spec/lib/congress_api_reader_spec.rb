4182d3fa37192e4a5f0cf9fc61d92b7d5dcb3e25
Franco Reyes <freyes@enova.com>
2014-08-21 16:13:33 -0500

    Additional tests created.
    Additional tests created. Also require base url passed when creating
    CongressAPIReader.

--- spec/lib/congress_api_reader_spec.rb
+++ spec/lib/congress_api_reader_spec.rb
@@ -2,33 +2,59 @@ require 'rails_helper'
 
 describe CongressAPIReader do
   let (:api_key) { '300952facb214f5983867ed073e7e4ba' }
-  let (:api_reader) { CongressAPIReader.new(api_key) }
+
+  let (:base_url) { 'https://congress.api.sunlightfoundation.com/' }
+  let (:api_reader) { CongressAPIReader.new(base_url, api_key) }
+
+  let (:bad_base_url) { 'https://congress.api.thisisabadurl.com/' }
+  let (:bad_api_reader) { CongressAPIReader.new(base_url, api_key) }
+
+  it "defaults to / when no argument given to get" do
+    api_reader.get("/")
+
+    expect(api_reader.results[0][:message]).to eq('I live!')
+    expect(api_reader.status).to eq(200)
+  end
 
   it "Is able to retrieve a response" do
-    puts api_key
     api_reader.get("/")
+
     expect(api_reader.results[0][:message]).to eq('I live!')
     expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve a single bill based on bill id" do
     api_reader.get("/bills", { :bill_id => "hr3461-111" } )
+
+    expect(api_reader.results[0][:bill_id]).to eq('hr3461-111')
     expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve many bills" do
     api_reader.get("/bills")
+
+    expect(api_reader.results.size).to gt(0)
     expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve a single legislator based on bioguide_id" do
     api_reader.get("/legislators", {:bioguide_id => "D000563"})
+
+    expect(api_reader.results[0][:bioguide_id]).to eq('D000563')
     expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve many legislators" do
     api_reader.get("/legislators")
     expect(api_reader.status).to eq(200)
+  end
+
+  it "Is able to handle being given a bad base url" do
+    bad_api_reader.get("/")
+
+    expect(bad_api_reader.data[:api_reader_success]).to eq(false)
+    expect(bad_api_reader.data[:api_reader_message]).to eq('Path not found')
+    expect(bad_api_reader.status).to eq(404)
   end
 
   it "Is able to let us know resource doesn't exist" do
