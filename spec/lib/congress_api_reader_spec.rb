214cb7c2002ff905daec7e003a4f019ebd97f271
Franco Reyes <freyes@enova.com>
2014-08-21 15:34:15 -0500

    Congress API reader tests
    Tests are a little more fleshed out

--- spec/lib/congress_api_reader_spec.rb
+++ spec/lib/congress_api_reader_spec.rb
@@ -7,23 +7,44 @@ describe CongressAPIReader do
   it "Is able to retrieve a response" do
     puts api_key
     api_reader.get("/")
-    expect(api_reader.data[:message]).to eq('I live!')
+    expect(api_reader.results[0][:message]).to eq('I live!')
+    expect(api_reader.status).to eq(200)
   end
 
-  it "Is able to retrieve a single bill" do
-    api_reader.get("/bills")
+  it "Is able to retrieve a single bill based on bill id" do
+    api_reader.get("/bills", { :bill_id => "hr3461-111" } )
+    expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve many bills" do
     api_reader.get("/bills")
+    expect(api_reader.status).to eq(200)
   end
 
-  it "Is able to retrieve a single legislator" do
-    api_reader.get("/legislators")
+  it "Is able to retrieve a single legislator based on bioguide_id" do
+    api_reader.get("/legislators", {:bioguide_id => "D000563"})
+    expect(api_reader.status).to eq(200)
   end
 
   it "Is able to retrieve many legislators" do
     api_reader.get("/legislators")
+    expect(api_reader.status).to eq(200)
+  end
+
+  it "Is able to let us know resource doesn't exist" do
+    api_reader.get("/dummy_path")
+
+    expect(api_reader.data[:api_reader_success]).to eq(false)
+    expect(api_reader.data[:api_reader_message]).to eq('Path not found')
+    expect(api_reader.status).to eq(404)
+  end
+
+  it "Is able to know there are no results" do
+    api_reader.get("/legislators", {:query => "Fitzgerald"})
+
+    expect(api_reader.data[:api_reader_success]).to eq(true)
+    expect(api_reader.data[:api_reader_message]).to eq('No results found')
+    expect(api_reader.status).to eq(200)
   end
 
 end
