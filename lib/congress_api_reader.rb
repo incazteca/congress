8d126a27be1d87a3afc5871da9e8743600e6200b
Franco Reyes <freyes@enova.com>
2014-08-22 10:34:46 -0500

    Updated test cases and fixed up CongressAPIReader
    Added handling for page option. So other pages can be selected. Also
    count and page number is returned by the CongressAPIReader object.

--- lib/congress_api_reader.rb
+++ lib/congress_api_reader.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 class CongressAPIReader
   attr_accessor :status, :data, :results
+  attr_reader :count, :page
   attr_writer :api_key, :base_url
 
   def initialize(base_url, api_key)
@@ -21,28 +22,41 @@ class CongressAPIReader
     query_string += "&query=#{options[:query] }" if options[:query] != nil
     query_string += "&bill_id=#{options[:bill_id] }" if options[:bill_id] != nil
     query_string += "&bioguide_id=#{options[:bioguide_id] }" if options[:bioguide_id] != nil
+    query_string += "&page=#{options[:page] }" if options[:page] != nil
 
     @data = Hash.new
 
     begin
       response = open(@base_url+path+api_key+per_page+query_string)
     rescue SocketError => sock_e
-      @data[:api_reader_message] = sock_e.message
-      @data[:api_reader_success] = false
+      set_data(sock_e.message, false)
     rescue OpenURI::HTTPError => http_e
-      @data[:api_reader_message] = http_e.message
-      @data[:api_reader_success] = false
+      set_data(http_e.message, false)
       @status = 404
     else
       @status = response.status[0].to_i
 
-      @results = Array.new
+      set_results(JSON.parse(response.read, symbolize_names: :true), path)
+      set_data('No results found', true) if @count == 0
+      set_data('Success', true) if @count > 0
+    end
+  end
 
-      @results[0] = JSON.parse(response.read, symbolize_names: :true) if path == '/'
-      @results = JSON.parse(response.read, symbolize_names: :true)[:results] if path != '/'
+  def set_data(message, success)
+      @data[:api_reader_message] = message
+      @data[:api_reader_success] = success
+  end
 
-      @data[:api_reader_success] = true
-      @data[:api_reader_message] = 'No results found' if @results.size == 0 or @results == nil
+  def set_results(results, path)
+    @results = Array.new
+    if path == '/'
+      @results[0] = results
+      @page = 0
+      @count = 0
+    else
+      @results = results[:results]
+      @page = results[:page][:page]
+      @count = results[:count]
     end
   end
 end
--- spec/lib/congress_api_reader_spec.rb
+++ spec/lib/congress_api_reader_spec.rb
@@ -9,25 +9,28 @@ describe CongressAPIReader do
   let (:bad_base_url) { 'https://congress.api.dummyapihttp.com' }
   let (:bad_api_reader) { CongressAPIReader.new(bad_base_url, api_key) }
 
+  let (:http_success)  { 200 }
+  let (:http_failure)  { 404 }
+
   it "defaults to / when no argument given to get" do
     api_reader.get
 
     expect(api_reader.results[0][:message]).to eq('I live!')
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to retrieve a response" do
     api_reader.get("/")
 
     expect(api_reader.results[0][:message]).to eq('I live!')
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to retrieve a single bill based on bill id" do
     api_reader.get("/bills", { :bill_id => "hr3461-111" } )
 
     expect(api_reader.results[0][:bill_id]).to eq('hr3461-111')
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to retrieve many bills" do
@@ -35,14 +38,29 @@ describe CongressAPIReader do
 
     expect(api_reader.results[0][:bill_id]).not_to be_nil
     expect(api_reader.results.size).to be > 0
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
+  end
+
+  it "Is able to get the result without need of counting the results" do
+    api_reader.get("/bills", { :bill_id => "hr3461-111" } )
+
+    expect(api_reader.count).to eq(1)
+    expect(api_reader.status).to eq(http_success)
+  end
+
+  it "Is able to go to other page numbers" do
+    pagenum = 2
+    api_reader.get("/bills", { :bill_id => "hr3461-111", :page => pagenum } )
+
+    expect(api_reader.page).to eq(pagenum)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to retrieve a single legislator based on bioguide_id" do
     api_reader.get("/legislators", {:bioguide_id => "D000563"})
 
     expect(api_reader.results[0][:bioguide_id]).to eq('D000563')
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to retrieve many legislators" do
@@ -50,7 +68,7 @@ describe CongressAPIReader do
 
     expect(api_reader.results[0][:bioguide_id]).not_to be_nil
     expect(api_reader.results.size).to be > 0
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
 
   it "Is able to handle being given a bad base url" do
@@ -66,7 +84,7 @@ describe CongressAPIReader do
 
     expect(api_reader.data[:api_reader_success]).to eq(false)
     expect(api_reader.data[:api_reader_message]).to eq('404 Not Found')
-    expect(api_reader.status).to eq(404)
+    expect(api_reader.status).to eq(http_failure)
   end
 
   it "Is able to know there are no results" do
@@ -74,7 +92,6 @@ describe CongressAPIReader do
 
     expect(api_reader.data[:api_reader_success]).to eq(true)
     expect(api_reader.data[:api_reader_message]).to eq('No results found')
-    expect(api_reader.status).to eq(200)
+    expect(api_reader.status).to eq(http_success)
   end
-
 end
