c43384e9cdbdbc0539e5bd6211373dc924480113
Franco Reyes <freyes@enova.com>
2014-08-25 16:28:30 -0500

    Seeding for Bills and Legislators table.
    Tables are seeded with the help of Congress API Reader

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+require 'pry'
+def create_bills(bills = [])
+  params = {}
+  bills.each do |bill|
+    Bill.column_names.each do |column|
+
+      if /^history_.*/ =~ column
+        col_split = column.split('_', 2)
+        params[column] = bill[col_split[0].to_sym][col_split[1].to_sym]
+      elsif column == 'related_bill_ids' || column == 'enacted_as'
+        next
+      else
+        params[column] = bill["#{column}".to_sym]
+      end
+    end
+
+    Bill.create(params)
+  end
+end
+
+def create_legislators(legislators = [])
+  params = {}
+  legislators.each do |legislator|
+    Legislator.column_names.each do |column|
+      params[column] = legislator["#{column}".to_sym]
+    end
+
+    Legislator.create(params)
+  end
+end
+
+base_url = 'https://congress.api.sunlightfoundation.com'
+api_key = '300952facb214f5983867ed073e7e4ba'
+
+api_reader = CongressAPIReader.new(base_url, api_key)
+
+max_per_page = 50
+
+# Begin populating bills
+
+page_num = 1
+
+loop do
+  api_reader.get("/bills", { :query => "highway", :page => page_num })
+
+  create_bills(api_reader.results)
+
+  page_num +=1
+  break if api_reader.page_count < max_per_page
+end
+
+# Begin populating Legislators
+
+page_num = 1
+
+loop do
+  api_reader.get("/legislators", { :page => page_num })
+
+  create_legislators(api_reader.results)
+
+  page_num +=1
+  break if api_reader.page_count < max_per_page
+end
+
