665de0652567fddeac5cb58eb1cde38d8e763fc8
Franco Reyes <freyes@enova.com>
2014-09-08 18:26:30 -0500

    Make search case insensitive
    Searching is now case insensitive. All queries in back made in lowercase
    except when searching bioguide_IDs. Those are put in uppercase.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,16 @@
 class SearchController < ApplicationController
   def index
 
-    search_term = params[:search_term].strip
+    search_term = params[:search_term].strip.downcase
 
     # Redirect to bill if search seems like a bill id
-    redirect_to bill_path(Bill.find_by(bill_id: search_term.downcase)) and return if /(s|hr)\d{1,4}-\d{1,3}/ =~ search_term.downcase
+    redirect_to bill_path(Bill.find_by(bill_id: search_term)) and return if /(s|hr)\d{1,4}-\d{1,3}/ =~ search_term
 
-    # Redirect to legislators if search seems like a bioguide_id
+    # Redirect to legislators if search seems like a bioguide_id, only time upcase needed
     redirect_to legislator_path(Legislator.find_by(bioguide_id: search_term.upcase)) and return if /[A-Z]\d{6}/ =~ search_term.upcase
 
-    @legislators = Legislator.where('first_name LIKE ? OR last_name LIKE ?', "%#{search_term}%", "%#{search_term}%")
-    @titles = Title.where('title LIKE ?', "%#{search_term}%")
+    @legislators = Legislator.where('lower(first_name) LIKE ? OR lower(last_name) LIKE ?', "%#{search_term}%", "%#{search_term}%")
+    @titles = Title.where('lower(title) LIKE ?', "%#{search_term}%")
     @bills = Bill.find(@titles.map(&:bill_id)) unless @titles.nil?
 
     redirect_to legislator_path(@legislators.first) and return if @legislators.count == 1
