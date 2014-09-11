fee406b16bf492c0f660eb091c0205d3b903535c
Franco Reyes <freyes@enova.com>
2014-09-11 10:18:10 -0500

    Added front-end portion for registration
    Added Registration form to application and link to reach it. This is
    only the front end portion the backend has yet to be implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :bills
   resources :legislators
+  resources :registration
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
