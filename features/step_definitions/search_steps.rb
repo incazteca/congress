fe24edbbae310c4a542524993648ef5206bec50f
Franco Reyes <freyes@enova.com>
2014-09-15 15:46:33 -0500

    Search Cuke updated to build needed objects
    Titles and their types are being created. Also organized the file
    starting with Givens, whens in the middle, and thens last.

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,4 +1,10 @@
+# Givens
+
 Given(/^User is on site with only one bill$/) do
+  create :official_type
+  create :short_type
+  create :popular_type
+  create :title
   create :bill
   visit '/'
 end
@@ -9,20 +15,22 @@ Given(/^User is on site with only one legislator$/) do
 end
 
 Given(/^User is on site with one legislator and one bill$/) do
-  create :bill
+  create :official_type
+  create :short_type
+  create :popular_type
   create :title
+  create :bill
   create :legislator
   visit '/'
 end
+
+# Whens
 
 When(/^User searches by bill id$/) do
   fill_in 'search_term', with: 'hr3461-111'
   click_button 'Search'
 end
 
-Then(/^User should be taken to page for that bill$/) do
-  expect(page).to have_content('hr3461-111')
-end
 
 When(/^User searches by bioguide id$/) do
   fill_in 'search_term', with: 'B000574'
@@ -38,22 +46,28 @@ When(/^User searches by title$/) do
   click_button 'Search'
 end
 
-Then(/^User should be taken to result page with relevant bills$/) do
-  expect(page).to have_content('Results')
-end
-
 When(/^User searches with unsearchable string$/) do
   fill_in 'search_term', with: 'plaintains'
   click_button 'Search'
 end
 
-Then(/^User should be taken to result page with no results message$/) do
-  expect(page).to have_content('No results found for plaintains. Please try another search.')
-end
-
 When(/^User searches without filling search field$/) do
   fill_in 'search_term', with: ''
   click_button 'Search'
+end
+
+# Thens
+
+Then(/^User should be taken to page for that bill$/) do
+  expect(page).to have_content('hr3461-111')
+end
+
+Then(/^User should be taken to result page with relevant bills$/) do
+  expect(page).to have_content('Results')
+end
+
+Then(/^User should be taken to result page with no results message$/) do
+  expect(page).to have_content('No results found for plaintains. Please try another search.')
 end
 
 Then(/^User should be taken to result page with default results$/) do
