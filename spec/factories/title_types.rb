10bea497d4046af29e07dbce0a9aa52131e9a26d
Franco Reyes <freyes@enova.com>
2014-09-15 16:56:38 -0500

    Specified ids for titles types in factory
    Title type ids specified so that we are not having issues with titles
    trying to lookup the types since the type id's will be out of range.

--- spec/factories/title_types.rb
+++ spec/factories/title_types.rb
@@ -2,14 +2,17 @@ FactoryGirl.define do
   factory :title_type do
 
     factory :official_type do
+      title_type_id 1
       title_type "official"
     end
 
     factory :short_type do
+      title_type_id 2
       title_type "short"
     end
 
     factory :popular_type do
+      title_type_id 3
       title_type "popular"
     end
 
