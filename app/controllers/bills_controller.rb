4190159be8d9a14c5e0c4c9ad017dc436c29f9a0
Franco Reyes <freyes@enova.com>
2014-09-17 16:47:13 -0500

    Edit bills and legislator controllers for styling
    Shorten the length of lines in bills and legislator controllers so that
    we don't brush up against the 80 col so much.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -4,11 +4,13 @@ class BillsController < ApplicationController
   layout 'application'
 
   def index
-    @bills = Bill.order("introduced_on desc").paginate(page: params[:page], per_page: 10)
+    @bills = Bill.order("introduced_on desc").
+      paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @bill = Bill.find(params[:id])
+    @tags = Tag.find_by_user_id(current_user.id)
   end
 
 end
--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -4,11 +4,13 @@ class LegislatorsController < ApplicationController
   layout 'application'
 
   def index
-    @legislators = Legislator.order("last_name, first_name").paginate(page: params[:page], per_page: 10)
+    @legislators = Legislator.order("last_name, first_name").
+      paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @legislator = Legislator.find(params[:id])
-    @bills = @legislator.bills.order("introduced_on desc").paginate(page: params[:page], per_page: 10)
+    @bills = @legislator.bills.order("introduced_on desc").
+      paginate(page: params[:page], per_page: 10)
   end
 end
