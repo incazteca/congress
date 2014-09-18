656ed5b52f718637430ce1423ae83dacc252b3c9
Franco Reyes <freyes@enova.com>
2014-09-18 14:57:59 -0500

    Tag button set up call for untag and tag
    Setting up the tag button to send the right parameters when it calls the
    tag or untag method.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -36,9 +36,18 @@ class Bill < ActiveRecord::Base
   end
 
   def tagged?(user)
+    return if user.nil?
     tags = Tag.where('user_id = ? AND tag_type_id = ?',
                      user.id, TagType.find_by(tag_type: 'bill'))
 
     tags.map(&:tagged_item_id).include? id
+  end
+
+  def tag_id(user)
+    return if user.nil?
+    tag = Tag.where('user_id = ? AND tag_type_id = ? AND tagged_item_id = ?',
+                     user.id, TagType.find_by(tag_type: 'bill'), id).first
+
+    return tag.id unless tag.nil?
   end
 end
--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -9,9 +9,18 @@ class Legislator < ActiveRecord::Base
   end
 
   def tagged?(user)
+    return if user.nil?
     tags = Tag.where('user_id = ? AND tag_type_id = ?',
                      user.id, TagType.find_by(tag_type: 'legislator'))
 
     tags.map(&:tagged_item_id).include? id
+  end
+
+  def tag_id(user)
+    return if user.nil?
+    tag = Tag.where('user_id = ? AND tag_type_id = ? AND tagged_item_id = ?',
+                     user.id, TagType.find_by(tag_type: 'legislator'), id).first
+
+    return tag.id unless tag.nil?
   end
 end
