b9e87e0d057b561f4f6fd6ba94ce02e9130bf8f8
Franco Reyes <freyes@enova.com>
2014-09-26 14:42:55 -0500

    Congress API Reader has been rubocopped...
    Yeah... sorry explicit returns.... Thanks Rubocop!

--- lib/congress_api_reader.rb
+++ lib/congress_api_reader.rb
@@ -11,13 +11,12 @@ class CongressAPIReader
   end
 
   def get(path = '/', options = {})
-
-    response = open(@base_url+path+query_string(options))
+    response = open(@base_url + path + query_string(options))
 
     status_ar = response.status
 
     results = JSON.parse(response.read, symbolize_names: true) if path != '/'
-    results = [ JSON.parse(response.read, symbolize_names: true) ] if path == '/'
+    results = [JSON.parse(response.read, symbolize_names: true)] if path == '/'
 
     response_data(path, results, status_ar[0].to_i, status_ar[1])
 
@@ -30,7 +29,6 @@ class CongressAPIReader
   private
 
   def response_data(path, results, status, message)
-
     response = Struct.new(:path, :http_results, :http_status, :http_message) do
       def results
         return http_results if path == '/'
@@ -38,48 +36,47 @@ class CongressAPIReader
       end
 
       def status
-       http_status
+        http_status
       end
 
       def message
-        return 'No results found' if count == 0 && http_results != nil
+        return 'No results found' if count == 0 && !http_results.nil?
         http_message
       end
 
       def page
-        return 0 if http_results == nil
+        return 0 if http_results.nil?
         http_results[:page][:page]
       end
 
       def per_page
-        return 0 if http_results == nil
+        return 0 if http_results.nil?
         http_results[:page][:per_page]
       end
 
       def count_on_page
-        return 0 if http_results == nil
+        return 0 if http_results.nil?
         http_results[:page][:count]
       end
 
       def count
-        return 0 if http_results == nil
+        return 0 if http_results.nil?
         http_results[:count]
       end
     end
 
-    return response.new(path, results, status, message)
+    response.new(path, results, status, message)
   end
 
   def query_string(options)
     query_string = "?apikey=#{@api_key}"
 
-    query_string += "&per_page=50" if options[:per_page] == nil
+    query_string += '&per_page=50' if options[:per_page].nil?
 
-    options.each do |k,v|
-      query_string += "&#{k.to_s}=#{v}"
+    options.each do |k, v|
+      query_string += "&#{k}=#{v}"
     end
 
     query_string
   end
-
 end
