0e8157fc72d9d7d3c2d5eeabfab3f1b2ad97073e
Franco Reyes <freyes@enova.com>
2014-09-26 12:35:07 -0500

    Use of bill and title factories in rspec
    Using factories in bill model test

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -4,15 +4,27 @@ RSpec.describe Bill, :type => :model do
   let (:bill) { Bill.find_by(bill_id: 'hr2642-113') }
 
   it "has an official title" do
-    expect(bill.official_title).not_to be_nil
+    create(:official_type)
+    create(:short_type)
+    create(:popular_type)
+    create(:title)
+    expect(build(:bill).official_title).not_to be_nil
   end
 
   it "has a short title" do
-    expect(bill.short_title).not_to be_nil
+    create(:official_type)
+    create(:short_type)
+    create(:popular_type)
+    create(:title)
+    expect(build(:bill).short_title).not_to be_nil
   end
 
   it "has a popular title" do
-    expect(bill.popular_title).not_to be_nil
+    create(:official_type)
+    create(:short_type)
+    create(:popular_type)
+    create(:title)
+    expect(build(:bill).popular_title).not_to be_nil
   end
 
 end
